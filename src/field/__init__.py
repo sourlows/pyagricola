5a6f5b1a4e199f9aeef5f113ac26962fbf83ecc6
Dustin Walker <dnelson14@gmail.com>
2015-02-07 02:34:26 -0600

    field nodes

--- src/field/__init__.py
+++ src/field/__init__.py
@@ -4,27 +4,38 @@ __author__ = 'Eliniel'
 Classes and methods that control the behaviour of a playing field
 """
 
+
+class FieldNode(object):
+    def __init__(self, label, item=None):
+        self.label = label
+        self.item = item
+
+    def update(self):
+        """ Called at the beginning of each turn """
+        if self.item:
+            self.item.update()
+
+
 class Field(object):
 
     def __init__(self):
         self.field = graph()
+        self.matrix = [[0 for x in xrange(7)] for x in xrange(5)]
         
         # Create field nodes in graph
         for y in xrange(0, 5):
             for x in xrange(0, 7):
                 index_string = '%s%s' % (y, x)
-                self.field.add_node(index_string)
+                node = FieldNode(label=index_string)
+                self.field.add_node(node)
+                self.matrix[y][x] = node
 
         # Horizontal edges
         for y in xrange(0, 5):
             for x in xrange(0, 6):
-                firstnode = '%s%s' % (y, x)
-                secondnode = '%s%s' % (y, x+1)
-                self.field.add_edge((firstnode, secondnode))
+                self.field.add_edge((self.matrix[y][x], self.matrix[y][x+1]))
 
         # Vertical edges
         for x in xrange(0, 7):
             for y in xrange(0, 4):
-                firstnode = '%s%s' % (y, x)
-                secondnode = '%s%s' % (y+1, x)
-                self.field.add_edge((firstnode, secondnode))
+                self.field.add_edge((self.matrix[y][x], self.matrix[y+1][x]))
